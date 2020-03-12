91d20b6d7f6bda38989e4fd15f8bb82c093e52e6
fernandojvlima <fernandojvlima@gmail.com>
2020-03-12 15:52:39 -0300

    Series

--- frontend/src/pages/Details/index.js
+++ frontend/src/pages/Details/index.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { connect } from 'react-redux';
+import './index.css'
+import {
+  Card, CardImg, CardBody, CardTitle, Jumbotron
+} from 'reactstrap';
+import { imageExtension } from '../../services/api'
 
 const Details = (props) => {
-
   const parametrosDaUrl = window.location.href;
   const res = parametrosDaUrl.split('/')
   const id = Number(res[4])
-
-
-
-  const characterPicked = props.data.filter((item) => {
-    return item.id === id
-  }
-  )
-
-  console.log({ characterPicked })
-
-
-
-
+  const characterPicked = props.data.filter(item => item.id === id)
+  console.log(characterPicked)
   return (
     <div>
       <Header />
-      <h1>Detalhes</h1>
-      <pre>{characterPicked.props}</pre>
+      {characterPicked.map((item) => {
+        return (
+          <div className="container" key={item.id}>
+            <Card className="card-corpo-details" key={item.id}>
+              <CardImg top src={item.thumbnail.path + imageExtension} alt={item.name} className="card-image" />
+              <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <p className="card-text">Última Atualização: {item.modified}</p>
+                <p className="card-text">Descrição: {item.description === '' ? 'Personagem não possui descrição' : item.description}</p>
+              </CardBody>
+              <Jumbotron className="Jumbo">
+                <h6>Séries que o {item.name} participou :</h6>
+                {item.series.items.map((serie) => {
+                  return (
+                    <div key={serie.name}>
+                      <div className="alert alert-info" role="alert">
+                        {serie.name}
+                      </div>
+                    </div>
+                  )
+                })}
+              </Jumbotron>
+            </Card>
+
+
+
+
+
+          </div>
+        )
+      })}
     </div>
   )
 }
